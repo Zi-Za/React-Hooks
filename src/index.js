d3ff103990b3557951572d7d3557a20925341c30
Zina Zadorozhnaya <Zadorozhnaya@mail.ru>
2023-03-01 15:44:32 +0700

    continue project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from 'routes'
+import TopBar from 'components/topBar'
+
+const App = () => {
+  return (
+    <div>
+        <BrowserRouter>
+          <TopBar/>
+          <Routes/>
+        </BrowserRouter>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
